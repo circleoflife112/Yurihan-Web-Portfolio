1a96ea3b4c35903920704478da0e773bf1071ee4
Yuri Han <yurihan77@google.com>
2024-10-02 16:37:24 +0900

    correct img

--- doosi.js
+++ doosi.js
@@ -2,16 +2,17 @@ const initSlider = () => {
   const slideButton = document.querySelectorAll("button");
   const imageList = document.getElementById("postcard-design-container");
   const slideList = document.querySelectorAll("#postcard-design-container li");
+  const margin = 40;
 
-  slideButton.forEach(button => {
+  slideButton.forEach((button) => {
     button.addEventListener("click", () => {
       const direction = button.id === "pre-button" ? -1 : 1;
       const scrollAmount = slideList[0].clientWidth * direction;
-      imageList.scrollBy({ left: scrollAmount, behavior: "smooth"});
+      imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
     });
   });
-}
+};
 
 window.addEventListener("load", initSlider);
 
-// 
+//
