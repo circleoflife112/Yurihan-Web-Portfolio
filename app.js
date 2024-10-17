56246eda3f919aa8ad5dc26a285a1dd9388066e8
Yuri Han <yurihan77@google.com>
2024-10-17 15:37:47 +0900

    correct main page

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+const mainPage = document.getElementById("container");
 const ul = document.getElementById("rope_holder");
-const yellowDelma = document.getElementsByClassName("yellow-delma");
-const delmaAnchor = document.getElementsByClassName("yellow-thread");
+const thread = document.querySelectorAll("#rope_holder li");
 
-console.log(delmaAnchor);
+console.log(thread);
+
+const hambergurHolder = document.querySelectorAll(".aside_btn");
+const asidePage = document.getElementById("side-drawer");
+
+function occurAside() {
+  asidePage.classList.toggle("active");
+  asidePage.classList.toggle("introduce_page");
+}
+
+hambergurHolder[0].addEventListener("click", occurAside);
+hambergurHolder[1].addEventListener("click", occurAside);
+
+console.log(hambergurHolder);
